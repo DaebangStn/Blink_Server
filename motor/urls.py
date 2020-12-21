0f882bd1d5650f121184aec281a8fe6a9a80e78d
DaebangStn <gunho1107@naver.com>
2020-12-21 09:08:07 +0900

    url pattern modified

--- motor/urls.py
+++ motor/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('update/', views.update, name='update'),
-    re_path(r'^control/(?P<parm_mac>[\s\S])/(?P<parm_mode>[od].{1,})$', views.mode, name='mode'),
+    re_path(r'^control/(?P<parm_mac>[\s\S])/(?P<parm_mode>[od].+)$', views.mode, name='mode'),
     re_path(r'^control/(?P<parm_mac>[\s\S])/(?:speed-(?P<parm_speed>[0-9]*))$', views.change_speed, name='change_speed'),
 ]
